b6251b27feca4829b03d9708d35617f70fe98dcb
devletts <bluecamel84@gmail.com>
2019-06-18 15:11:35 +0930

    update

--- main.py
+++ main.py
@@ -41,12 +41,3 @@ def test_SVDD():
 
 test_SVDD()
 
-'''
-tool = toolkit()
-#set options
-options = tool.setAlgorthem("deepsvdd")
-options.datasetname = "mnist"
-options.class_num = 4
-tool.train(options)
-tool.test()
-'''
